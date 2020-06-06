0dffcae13cc4f596da7d201cdcaeab857e422b21
kon104 <toshiharu@MacBook2017.local>
2020-06-06 21:39:12 +0900

    added a controller at portal url

---
+++ src/main/java/com/herokuapp/kon104/webapp/controller/PortalController.java
@@ -0,0 +1,17 @@
+package com.herokuapp.kon104.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Portal Controller
+ */
+@Controller
+public class PortalController
+{
+	@RequestMapping("/")
+	public String index()
+	{
+		return "redirect:/printaddr/";
+	}
+}
