7595d12af04fee90d4e6fe7f8d6f257924b8697e
kon104 <toshiharu@MacBook2017.local>
2020-06-11 01:01:25 +0900

    improved sitemap

--- src/main/java/com/herokuapp/kon104/webapp/controller/SiteMapController.java
+++ src/main/java/com/herokuapp/kon104/webapp/controller/SiteMapController.java
@@ -1,25 +1,33 @@
 package com.herokuapp.kon104.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import com.herokuapp.kon104.webapp.domain.SiteMapService;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+
 /**
  * Site Map Controller
  */
-@RestController
+//	@RestController
+@Controller
+@RequestMapping("/")
 public class SiteMapController
 {
 	@Autowired
 	private SiteMapService service;
 
 	// {{{ public String sitemap(HttpServletRequest request)
-	@RequestMapping(value = "/sitemap.xml", produces = "text/xml")
+//	@RequestMapping(value = "/sitemap.xml", produces = "text/xml")
+	@GetMapping(value = "/sitemap.xml", produces = MediaType.APPLICATION_XML_VALUE)
 	public String sitemap(HttpServletRequest request)
 	{
-		return service.getSiteMapXml(request);
+//		return service.getSiteMapXml(request);
+		return "portal/sitemap";
 	}
 	// }}}
 
