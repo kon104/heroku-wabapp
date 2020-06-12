5db42ab30598286e91ae66edc8cd880e264202e8
kon104 <toshiharu@MacBook2017.local>
2020-06-13 03:08:23 +0900

    updated sitemap

--- src/main/java/com/herokuapp/kon104/webapp/controller/PortalController.java
+++ src/main/java/com/herokuapp/kon104/webapp/controller/PortalController.java
@@ -1,7 +1,13 @@
 package com.herokuapp.kon104.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
+import com.herokuapp.kon104.webapp.domain.SiteMapService;
 
 /**
  * Portal Controller
@@ -9,9 +15,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PortalController
 {
+	@Autowired
+	private SiteMapService service;
+
+	// {{{ public String index()
 	@RequestMapping("/")
 	public String index()
 	{
 		return "redirect:/printaddr/";
 	}
+	// }}}
+
+	// {{{ public String sitemap(HttpServletRequest request)
+	@GetMapping(value = "/sitemap.xml", produces = MediaType.TEXT_XML_VALUE)
+	public String sitemap(HttpServletRequest request, Model model)
+	{
+		model.addAttribute("sitemaps", service.getSiteMap(request));
+		return "portal/sitemap";
+	}
+	// }}}
+
 }
