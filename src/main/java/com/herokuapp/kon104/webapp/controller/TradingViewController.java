ef10880dec4e7b5411412466f80b24d2696753e5
kon104 <tkonuma@R-SUR-L4BL-0559.localdomain>
2022-08-20 17:34:43 +0900

    created the app to builtin the widget to use TradingView

---
+++ src/main/java/com/herokuapp/kon104/webapp/controller/TradingViewController.java
@@ -0,0 +1,35 @@
+package com.herokuapp.kon104.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/tradingview")
+public class TradingViewController
+{
+
+	// {{{ public String index()
+	@GetMapping("/")
+	public String index(
+		HttpServletRequest request,
+		@RequestParam(name = "m", required = false) String market,
+		@RequestParam(name = "c", required = false) String code,
+		Model model)
+	{
+		if (market == null) {
+			market = "NASDAQ";
+		}
+		if (code == null) {
+			code = "GOOGL";
+		}
+		model.addAttribute("market", market);
+		model.addAttribute("code", code);
+		return "tradingview/index";
+	}
+	// }}}
+
+}
